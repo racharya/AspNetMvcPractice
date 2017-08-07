018188e4e1675a676aeb060ed8ebad43de29f82c
Rachana Acharya <rachanaacharya@Rachanas-MacBook-Pro.local>
2017-08-06 23:46:11 -0600

    Added a new route

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,10 +12,19 @@ namespace Vidly
     public static void RegisterRoutes(RouteCollection routes)
     {
       routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
       routes.MapRoute(
+        name: "RandomMovies", 
+        url: "Movies/Random",
+        defaults: new {controller = "Movies", action = "Random"}
+      );
+      routes.MapRoute(
+          // 2nd part is url pattern which calls controller.action() if no id supplied
+          // controller.action(id) if id is supplied
           name: "Default",
           url: "{controller}/{action}/{id}",
+          // if url does not contain any of the url pattern, Home is called
+          // if url has only controller, it will call the controller.index
+          // e.g if url is /movies ==> MoviesController.Index()
           defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
       );
     }
